a13376d20bf9d9d8831749d9a1ba81cb95c45c07
luke <lukehtravis@gmail.com>
2017-11-01 11:15:58 -0700

    Added iterator to shelves so that passed in state will be output as titles

--- src/Shelf.js
+++ src/Shelf.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 // import * as BooksAPI from './BooksAPI'
 
 class Shelf extends Component {
+  static propTypes = {
+    shelves: PropTypes.array
+  }
+
   render() {
     return (
-      <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            <li>
-            </li>
-          <ol />
-        </div>
-      <div />
+      {this.props.shelves.map((shelf) => (
+        <div className="bookshelf">
+          <h2 className="bookshelf-title">{shelf}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              <li>
+              </li>
+            <ol />
+          </div>
+        <div />
+      ))}
     )
   }
 }
