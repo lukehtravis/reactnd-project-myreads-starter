7201cfb1c7b0c63b9ca3517f30f248a0597321c2
luke <lukehtravis@gmail.com>
2017-11-01 09:52:34 -0700

    Added some html to shelf.js

--- src/Shelf.js
+++ src/Shelf.js
@@ -4,7 +4,15 @@ import React, { Component } from 'react';
 class Shelf extends Component {
   render() {
     return (
-
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+            </li>
+          <ol />
+        </div>
+      <div />
     )
   }
 }
